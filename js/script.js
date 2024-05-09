45d1c80222d4425465178c5c3c6bcb8f9dd15194
devindahsari <devindahsari21@gmail.com>
2024-05-09 21:55:29 +0700

    Slideshow

--- js/script.js
+++ js/script.js
@@ -22,3 +22,31 @@ function Validation()
 }
 
 document.getElementById('send-btn').addEventListener('click', () => Validation())
+
+//init
+let indexSlide = 1;
+showSlide(1);
+
+function nextSlide(n){
+    console.log('nextSlide' + n)
+    showSlide(indexSlide += n);
+}
+//Slide Logic
+function showSlide(index){
+    let listImage = document.getElementsByClassName('mySlide');
+    
+    //Reset slide index
+    if (index > listImage.length) indexSlide = 1;
+
+    //Hide all image
+    let i = 0;
+    while (i < listImage.length){
+        listImage[i].style.display = 'none';
+        i++;
+    }
+
+    //Show only one image
+    listImage[indexSlide - 1].style.display= 'block';
+}
+
+setInterval(() => nextSlide(1),2000);
